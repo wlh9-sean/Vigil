53ab0c3c9853d738e581add611ad27a8300261ae
Sean Schouten <seanschouten@Seans-MacBook-Pro.local>
2019-09-12 10:27:21 -0600

    First commit

---
+++ server/index.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+const express = require('express')
+const massive = require('massive')
+const session = require('express-session')
+const cors = require('cors')
+const chalk = require('chalk')
+
+const {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET} = process.env
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+app.use(session({
+    resave: false,
+    saveUninitialized: true,
+    secret: SESSION_SECRET,
+    cookie: {
+        maxAge: 60000000000
+    }
+}))
+
+massive(CONNECTION_STRING).then(db => {
+    app.set('db', db)
+    console.log(chalk.blue('Database Connected 🦄'))
+}).catch(error => console.log(chalk.bgRed('Connection failed', error)))
+
+app.listen(SERVER_PORT, () => console.log(chalk.cyan(`Serving on port ${SERVER_PORT} 🚀`)))
